3ac419df4a378f79c3d9a1ffa3aef6150f5c765f
Sergio <sergiojuniorce@gmail.com>
2021-05-13 16:05:06 -0700

    removed out

--- src/pruebas/Pruebas.java
+++ src/pruebas/Pruebas.java
@@ -10,9 +10,6 @@ import org.bson.types.ObjectId;
 public class Pruebas {
 
     public static void main(String args[]) {
-        HabitatDAO dao = new HabitatDAO();
-        List<String> continentes = Arrays.asList("Oceania", "America");
-        dao.create(new Habitat("Selva", "Humedo", "Forndoza", continentes));
-        System.out.println(dao.get(new ObjectId("609dac45edf35fa6bb5dcfc2")));
+        
     }
 }
